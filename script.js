4700329b1ef00ead986376a5c55f0d00e3729688
Luka Momcilovic <52632596+lukasinos@users.noreply.github.com>
2020-03-25 19:19:19 +0100

    Trimming

--- script.js
+++ script.js
@@ -14,54 +14,30 @@ button.focus();
 
 function colorChange() {
   randomNumber = (Math.floor(Math.random() * (999-100)+100));
-	document.body.style.backgroundColor = "#" + randomNumber;
+  document.body.style.backgroundColor = "#" + randomNumber;
   button.value = "#" + randomNumber;
 }
 
-function pickColor(event)
-{
-  if(button.value === defaultTextOnSign)
-    {
+function pickColor(event){
+  if(button.value === defaultTextOnSign){
       button.value = "#";
     }
-  else
-    {
-      return false;
-    }
+  else return false;
 }
 
-function writeColor()
-{
-  if(button.value === defaultTextOnSign)
-    {
-     button.value = "fuck u";
-    }
-  else if(button.value.includes("#") === false)
-    {
+function writeColor(){
+  if(button.value === defaultTextOnSign) button.value = "fuck u";
+  else if(button.value.includes("#") === false){
       button.value = "#" + button.value;
       document.body.style.backgroundColor = button.value;
     }
-  else
-    {
-      document.body.style.backgroundColor = button.value;
-    }
+  else document.body.style.backgroundColor = button.value;
 }
 
-function def(event)
-{
-  if(event.target.id !== "color-button")
-    {
-      if(button.value === "#")
-			{	
-        button.value = defaultTextOnSign;
-   		}
-      else
-      {
-        return false;
-      } 
+function def(event){
+  if(event.target.id !== "color-button"){
+      if(button.value === "#") button.value = defaultTextOnSign;
+      else return false;
     }
-  else
-    {
-      return false;
-    }
+  else return false;
 }
